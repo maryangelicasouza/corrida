3a74f8b50f9cf240694f448b12e097d5e43aba99
maria <mariaangelicafreitas18@gmail.com>
2024-11-01 09:00:32 -0300

    imagens

--- corrida/MainPage.xaml.cs
+++ corrida/MainPage.xaml.cs
@@ -2,14 +2,71 @@
 
 public partial class MainPage : ContentPage
 {
-		public MainPage()
+	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-}
-	
 
-	
+	bool estaMorto = false;
+	bool estaPulando = false;
+
+	const int tempoEntreFrames = 25;
+	int velocidade1 = 0;
+	int velocidade2 = 0;
+	int velocidade3 = 0;
+	int velocidade = 0;
+	int LarguraJanela = 0;
+	int alturaJanela = 0;
+
+	protected override void OnSizeAllocated(double w, double h)
+	{
+		base.OnSizeAllocated(w, h);
+		CorrigeTamanhoCenario(w, h);
+		CalculaVelocidade(w);
+	}
+
+
+	void CalculaVelocidade(double w)
+
+	{
+		velocidade1 = (int)(w * 0.0001);
+		velocidade2 = (int)(w * 0.0004);
+		velocidade3 = (int)(w * 0.0008);
+		velocidade = (int)(w * 0.01);
+
+	}
+
+	void CorrigeTamanhoCenario(double w, double h)
+	{
+		foreach (var a in satack1.Children)
+			(a as Image).WidthRequest = w;
+		foreach (var a in satack2.Children)
+			(a as Image).WidthRequest = w;
+		foreach (var a in satack3.Children)
+			(a as Image).WidthRequest = w;
+		foreach (var a in satackchao.Children)
+			(a as Image).WidthRequest = w;
+
+		satack1.WidthRequest = w * 1.5;
+		satack2.WidthRequest = w * 1.5;
+		satack3.WidthRequest = w * 1.5;
+		satackchao.WidthRequest = w * 1.5;
+
+
+
+
+
+	}
+
+
+
+
+
+
+}
+
+
+
 
 
